b214df6cf051f5547a6c05f7655788b3e5c58315
toycucumber <ilimaksim@ya.ru>
2016-10-05 20:05:42 +0500

    sourdough proportions as REPL function

---
+++ src/sourdough/core.clj
@@ -0,0 +1,23 @@
+(ns sourdough.core
+  (:require [clojure.pprint :as pprint]
+            [clojure.java.io :as io]))
+
+(defn how-much-with-starter [starter]
+  (let
+    [first-time-sourdough (* 3 starter)
+     fin-sourdough (* 9 starter)
+     sourdough-in-dough (- fin-sourdough starter)
+     flour (* 11 (/ sourdough-in-dough 15.0))
+     water (/ sourdough-in-dough 3.0)
+     weight (+ sourdough-in-dough flour water)]
+    (clojure.pprint/print-table
+      [{"вторая подкормка по" first-time-sourdough
+        "в итоге закваски" fin-sourdough
+        "закваски в тесто" sourdough-in-dough
+        "муки в тесто"flour
+        "воды в тесто"water
+        "вес хлеба"weight}]))
+  )
+
+
+;; (how-much-with-starter 41)
